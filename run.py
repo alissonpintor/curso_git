ea79d8bfa6b8e4337193ae12a5b408973db34c36
Alisson Pintor <alissonpintor@gmail.com>
2017-01-17 09:52:40 -0300

    Alterado o arquivo run.py

--- run.py
+++ run.py
@@ -6,5 +6,4 @@ if __name__ == '__main__':
 		app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 	else:
 		app.run(host='localhost', port=8889, debug=True, reloader=True)
-
-#Servidor_Castelo_Ad; Servidor_Castelo_Blue; Servidor_Castelo_Blue_Antigo; Servidor_Castelo_RH; Ips_Liberados; Ips_Liberados_Stoky; 192.168.105.86;192.168.105.20
+		print('OK')
